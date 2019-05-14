cae0d21d0fe75aaadb9bc44d183cf5dd1ffd32b9
masdazaii <anggitprayoga@mail.ugm.ac.id>
2019-05-14 09:16:31 +0700

    my portofolio 1

---
+++ src/components/page/Contact.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing.unit * 2,
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+});
+
+
+
+class Contact extends React.Component{
+    
+    render(){
+        const { classes } = props;
+        return(
+            <div className={classes.root}>
+                <Grid container spacing={24}>
+                    <Grid item xs>
+                        <Paper className={classes.paper}>xs</Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs>
+                        <Paper className={classes.paper}>xs</Paper>
+                    </Grid>
+                </Grid>
+            </div>
+            
+        )
+    }
+}
+
+Contact.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles) (Contact);
